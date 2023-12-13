d26328734f0b1b722c46932f734db13750676dab
samuelkuria01 <samuel.kuria01@student.moringaschool.com>
2023-12-13 18:42:11 +0300

    responsiveness of the product page

--- src/Component/Products.js
+++ src/Component/Products.js
@@ -2,8 +2,168 @@ import React from 'react'
 
 function Products() {
   return (
-    <div>
+    <div className='productcontainer'>
+      {/* <div className='prodhead'>
       <h2>apa ni kwa products</h2>
+      </div> */}
+      
+      <div className='cardcont'>
+    
+      <div className='cardyyo'>
+        <img className='qasil' alt='blogim' src='https://5.imimg.com/data5/ANDROID/Default/2022/6/OV/NJ/JB/13210769/product-jpeg-500x500.jpg'></img>
+       <div className='qasilcontent'>
+       <p>qasil</p>
+       <div className='qasilbtn'>
+        <p><i class="fa-solid fa-circle-plus" ></i></p>
+        <p><i class="fa-regular fa-heart"></i></p>
+       </div>
+       </div>
+      </div>
+
+
+     <div className='cardyyo'>
+        <img className='qasil' alt='blogim' src='https://5.imimg.com/data5/ANDROID/Default/2022/6/OV/NJ/JB/13210769/product-jpeg-500x500.jpg'></img>
+       <div className='qasilcontent'>
+       <p>Bentonite Clay</p>
+       <div className='qasilbtn'>
+        <p><i class="fa-solid fa-circle-plus" ></i></p>
+        <p><i class="fa-regular fa-heart"></i></p>
+       </div>
+       </div>
+      </div>
+
+      <div className='cardyyo'>
+        <img className='qasil' alt='blogim' src='https://5.imimg.com/data5/ANDROID/Default/2022/6/OV/NJ/JB/13210769/product-jpeg-500x500.jpg'></img>
+       <div className='qasilcontent'>
+       <p>Tumeric</p>
+       <div className='qasilbtn'>
+        <p><i class="fa-solid fa-circle-plus" ></i></p>
+        <p><i class="fa-regular fa-heart"></i></p>
+       </div>
+       </div>
+      </div>
+
+
+      <div className='cardyyo'>
+        <img className='qasil' alt='blogim' src='https://5.imimg.com/data5/ANDROID/Default/2022/6/OV/NJ/JB/13210769/product-jpeg-500x500.jpg'></img>
+       <div className='qasilcontent'>
+       <p>Flax seeds</p>
+       <div className='qasilbtn'>
+        <p><i class="fa-solid fa-circle-plus" ></i></p>
+        <p><i class="fa-regular fa-heart"></i></p>
+       </div>
+       </div>
+      </div>
+
+
+      <div className='cardyyo'>
+        <img className='qasil' alt='blogim' src='https://5.imimg.com/data5/ANDROID/Default/2022/6/OV/NJ/JB/13210769/product-jpeg-500x500.jpg'></img>
+       <div className='qasilcontent'>
+       <p>Black Seeds</p>
+       <div className='qasilbtn'>
+        <p><i class="fa-solid fa-circle-plus" ></i></p>
+        <p><i class="fa-regular fa-heart"></i></p>
+       </div>
+       </div>
+      </div>
+
+
+      <div className='cardyyo'>
+        <img className='qasil' alt='blogim' src='https://5.imimg.com/data5/ANDROID/Default/2022/6/OV/NJ/JB/13210769/product-jpeg-500x500.jpg'></img>
+       <div className='qasilcontent'>
+       <p>Neem</p>
+       <div className='qasilbtn'>
+        <p><i class="fa-solid fa-circle-plus" ></i></p>
+        <p><i class="fa-regular fa-heart"></i></p>
+       </div>
+       </div>
+      </div>
+
+
+      <div className='cardyyo'>
+        <img className='qasil' alt='blogim' src='https://5.imimg.com/data5/ANDROID/Default/2022/6/OV/NJ/JB/13210769/product-jpeg-500x500.jpg'></img>
+       <div className='qasilcontent'>
+       <p>Moringa</p>
+       <div className='qasilbtn'>
+        <p><i class="fa-solid fa-circle-plus" ></i></p>
+        <p><i class="fa-regular fa-heart"></i></p>
+       </div>
+       </div>
+      </div>
+
+
+      <div className='cardyyo'>
+        <img className='qasil' alt='blogim' src='https://5.imimg.com/data5/ANDROID/Default/2022/6/OV/NJ/JB/13210769/product-jpeg-500x500.jpg'></img>
+       <div className='qasilcontent'>
+       <p>Hibiscus</p>
+       <div className='qasilbtn'>
+        <p><i class="fa-solid fa-circle-plus" ></i></p>
+        <p><i class="fa-regular fa-heart"></i></p>
+       </div>
+       </div>
+      </div>
+
+
+      <div className='cardyyo'>
+        <img className='qasil' alt='blogim' src='https://5.imimg.com/data5/ANDROID/Default/2022/6/OV/NJ/JB/13210769/product-jpeg-500x500.jpg'></img>
+       <div className='qasilcontent'>
+       <p>Goron Tula</p>
+       <div className='qasilbtn'>
+        <p><i class="fa-solid fa-circle-plus" ></i></p>
+        <p><i class="fa-regular fa-heart"></i></p>
+       </div>
+       </div>
+      </div>
+
+
+    <div className='cardyyo'>
+        <img className='qasil' alt='blogim' src='https://5.imimg.com/data5/ANDROID/Default/2022/6/OV/NJ/JB/13210769/product-jpeg-500x500.jpg'></img>
+       <div className='qasilcontent'>
+       <p>Acacia</p>
+       <div className='qasilbtn'>
+        <p><i class="fa-solid fa-circle-plus" ></i></p>
+        <p><i class="fa-regular fa-heart"></i></p>
+       </div>
+       </div>
+      </div>
+
+
+      <div className='cardyyo'>
+        <img className='qasil' alt='blogim' src='https://5.imimg.com/data5/ANDROID/Default/2022/6/OV/NJ/JB/13210769/product-jpeg-500x500.jpg'></img>
+       <div className='qasilcontent'>
+       <p>Shear Butter</p>
+       <div className='qasilbtn'>
+        <p><i class="fa-solid fa-circle-plus" ></i></p>
+        <p><i class="fa-regular fa-heart"></i></p>
+       </div>
+       </div>
+      </div>
+
+
+      <div className='cardyyo'>
+        <img className='qasil' alt='blogim' src='https://5.imimg.com/data5/ANDROID/Default/2022/6/OV/NJ/JB/13210769/product-jpeg-500x500.jpg'></img>
+       <div className='qasilcontent'>
+       <p>Fenugreek</p>
+       <div className='qasilbtn'>
+        <p><i class="fa-solid fa-circle-plus" ></i></p>
+        <p><i class="fa-regular fa-heart"></i></p>
+       </div>
+       </div>
+      </div>
+
+
+      <div className='cardyyo'>
+        <img className='qasil' alt='blogim' src='https://5.imimg.com/data5/ANDROID/Default/2022/6/OV/NJ/JB/13210769/product-jpeg-500x500.jpg'></img>
+       <div className='qasilcontent'>
+       <p>Henner nupur</p>
+       <div className='qasilbtn'>
+        <p><i class="fa-solid fa-circle-plus" ></i></p>
+        <p><i class="fa-regular fa-heart"></i></p>
+       </div>
+       </div>
+      </div>
+      </div>
+     
     </div>
   )
 }
